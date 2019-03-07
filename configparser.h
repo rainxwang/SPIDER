94eca6db35afe120024170e98c6a2cd394b6af0f
wangxinyu1997 <2216568381@qq.com>
2019-03-07 22:39:23 +0800

    开篇介绍

---
+++ configparser.h
@@ -0,0 +1,25 @@
+#ifndef CONFIGEPARSERR_H
+#define CONFIGEPARSERR_H
+
+
+
+typedef struct Config
+{
+	int 	max_job_num;//最大的任务数
+	char* 	seeds;//原始种子
+	char*	logfile;//日志文件名称
+	int 	log_level;//日志的等级
+	int 	max_depth;//每个url的最大深度
+	char*	module_path;//模块的路径
+	vector<char*>	modules;//哪些模块可以被加载
+	vector<char*>	accept_types;//保存的时候可以支持的类型
+}
+
+extern Config* initconfig();//配置文件的全局变量初始化
+extern void loadconfig(Config* conf);//读取配置文件
+
+
+
+
+
+#endif
---
+++ dso.h
@@ -0,0 +1,48 @@
+#ifndef DSO_H
+#define DSO_H
+
+#include <vector>
+
+#define MODULE_OK 0
+#define MODULE_ERR 1
+
+#define MAJOR_NUM 20190307
+#define MINOR_NUM 0
+
+#define STANDARD_MODULE_STUFF MAJOR_NUM, \
+								MINOR_NUM, \
+								__FILE__
+
+//无论什么模块都考虑因该有以下的信息
+typedef struct Module
+{
+	int version;
+	int minor_version;
+	const char* name;
+	void (*init)(Module*);//每个模块都有init初始化函数，这样动态载入的时候可以调用这个模块的函数进行初始化
+	int (*handle)(void *);//这个就相当于每个模块都有这么一个入口，
+} Module;
+
+//处理url的模块
+extern vector<Module*> modules_pre_surl;
+//载入这些url相关模块需要的初始化操作
+#define SPIDER_ADD_MODULE_PRE_SURL(module) do { \
+	modules_pre_surl.push_back(module); \
+} while(0)
+
+extern vector<Module*> modules_post_html;
+#define MODULES_POST_HTML(module) do { \
+	modules_post_html.push_back(module); \
+} while(0)
+
+
+extern vector<Module*> modules_post_header;
+#define MODULES_POST_HEADER(module) do { \
+	modules_post_header.push_back(module); \
+} while(0)
+
+//主流程需要调用的载入操作
+extern Modules* do_load(const char* path, const char* name);
+
+
+#endif
